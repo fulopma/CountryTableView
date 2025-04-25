69719fbd5bef2091f070c67341bed900d0cc01d6
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-04-25 13:04:56 -0400

    Initial Commit

---
+++ JsonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JsonApp
+//
+//  Created by Marcell Fulop on 4/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
